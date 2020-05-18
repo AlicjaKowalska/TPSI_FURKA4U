6ccb0e3c9eb2aa648e662a0e78370f17a1488612
alicj <alicjakowalska0624@gmail.com>
2020-05-18 22:38:51 +0200

    Lab11

--- src/main/java/wizut/tpsi/ogloszenia/HomeController.java
+++ src/main/java/wizut/tpsi/ogloszenia/HomeController.java
@@ -1,24 +1,63 @@
 package wizut.tpsi.ogloszenia;
 
 import java.sql.SQLException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wizut.tpsi.ogloszenia.jpa.BodyStyle;
 import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
+import wizut.tpsi.ogloszenia.jpa.CarModel;
+import wizut.tpsi.ogloszenia.jpa.FuelType;
+import wizut.tpsi.ogloszenia.jpa.Offer;
 import wizut.tpsi.ogloszenia.services.OffersService;
-
 
 @Controller
 public class HomeController {
+
     @Autowired
     private OffersService offersService;
-    
-     @RequestMapping("/")
-    public String home(Model model) throws SQLException {
-        model.addAttribute("carmanufacturer",offersService.getCarManufacturer(2));
-        model.addAttribute("carmodel",offersService.getModel(3));
+
+    @RequestMapping("/home")
+    public String hm(Model model) throws SQLException {
+        model.addAttribute("carmanufacturer", offersService.getCarManufacturer(2));
+        model.addAttribute("carmodel", offersService.getModel(3));
         return "index";
     }
-    
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        /*List<CarManufacturer> carManufacturers = offersService.getCarManufacturers();
+        List<BodyStyle> bodyStyles = offersService.getBodyStyles();
+        List<FuelType> fuelTypes = offersService.getFuelTypes();
+        List<CarModel> carModels = offersService.getCarModels();
+
+        model.addAttribute("carManufacturers", carManufacturers);
+        model.addAttribute("bodyStyles", bodyStyles);
+        model.addAttribute("fuelTypes", fuelTypes);
+        model.addAttribute("carModels", carModels);
+
+        return "offersList";*/
+        List<CarManufacturer> carManufacturers = offersService.getCarManufacturers();
+        List<CarModel> carModels = offersService.getCarModels();
+
+        List<Offer> offers = offersService.getOffers();
+
+        model.addAttribute("carManufacturers", carManufacturers);
+        model.addAttribute("carModels", carModels);
+        model.addAttribute("offers", offers);
+
+        return "offersList";
+    }
+
+    @GetMapping("/offer/{id}")
+    public String offerDetails(Model model, @PathVariable("id") Integer id) {
+        Offer offer = offersService.getOffer(id);
+        model.addAttribute("offer", offer);
+        return "offerDetails";
+    }
 }
--- src/main/java/wizut/tpsi/ogloszenia/services/OffersService.java
+++ src/main/java/wizut/tpsi/ogloszenia/services/OffersService.java
@@ -1,23 +1,107 @@
 package wizut.tpsi.ogloszenia.services;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import org.springframework.stereotype.Service;
+import wizut.tpsi.ogloszenia.jpa.BodyStyle;
 import wizut.tpsi.ogloszenia.jpa.CarManufacturer;
 import wizut.tpsi.ogloszenia.jpa.CarModel;
+import wizut.tpsi.ogloszenia.jpa.FuelType;
+import wizut.tpsi.ogloszenia.jpa.Offer;
 
 @Service
 public class OffersService {
-    
-     @PersistenceContext
+
+    @PersistenceContext
     private EntityManager em;
-     
-     public CarManufacturer getCarManufacturer(int id) {
+
+    public CarManufacturer getCarManufacturer(int id) {
         return em.find(CarManufacturer.class, id);
     }
-     
-     public CarModel getModel(int id){
-         return em.find(CarModel.class,id);
-     }
-     
+
+    public CarModel getModel(int id) {
+        return em.find(CarModel.class, id);
+    }
+
+    public Offer getOffer(int id) {
+        return em.find(Offer.class, id);
+    }
+
+    public BodyStyle getBodyStyle(int id) {
+        return em.find(BodyStyle.class, id);
+    }
+
+    public FuelType getFuelType(int id) {
+        return em.find(FuelType.class, id);
+    }
+
+    public CarModel getCarModel(int id) {
+        return em.find(CarModel.class, id);
+    }
+    
+    public List<CarManufacturer> getCarManufacturers() {
+        String jpql = "select cm from CarManufacturer cm order by cm.name";
+        TypedQuery<CarManufacturer> query = em.createQuery(jpql, CarManufacturer.class);
+        List<CarManufacturer> result = query.getResultList();
+        return result;
+        //lub
+        //return em.createQuery("select cm from CarManufacturer cm order by cm.name", CarManufacturer.class).getResultList();
+    }
+
+    public List<CarModel> getCarModels(int manufacturerId) {
+        String jpql = "select cm from CarModel cm where cm.manufacturer.id = :id order by cm.name";
+
+        TypedQuery<CarModel> query = em.createQuery(jpql, CarModel.class);
+        query.setParameter("id", manufacturerId);
+
+        return query.getResultList();
+    }
+
+    public List<Offer> getOffers() {
+        String jpql = "select off from Offer off order by off.id";
+        TypedQuery<Offer> query = em.createQuery(jpql, Offer.class);
+        List<Offer> result = query.getResultList();
+        return result;
+    }
+
+    public List<Offer> getOffersByModel(int modelId) {
+        String jpql = "select off from Offer off where off.model.id = :id order by off.title";
+
+        TypedQuery<Offer> query = em.createQuery(jpql, Offer.class);
+        query.setParameter("id", modelId);
+
+        return query.getResultList();
+    }
+
+    public List<Offer> getOffersByManufacturer(int manufacturerId) {
+        String jpql = "select off from Offer off where off.model.manufacturer.id = :id order by off.title";
+
+        TypedQuery<Offer> query = em.createQuery(jpql, Offer.class);
+        query.setParameter("id", manufacturerId);
+
+        return query.getResultList();
+    }
+
+    public List<BodyStyle> getBodyStyles() {
+        String jpql = "select bs from BodyStyle bs order by bs.name";
+        TypedQuery<BodyStyle> query = em.createQuery(jpql, BodyStyle.class);
+        List<BodyStyle> result = query.getResultList();
+        return result;
+    }
+
+    public List<FuelType> getFuelTypes() {
+        String jpql = "select ft from FuelType ft order by ft.name";
+        TypedQuery<FuelType> query = em.createQuery(jpql, FuelType.class);
+        List<FuelType> result = query.getResultList();
+        return result;
+    }
+
+    public List<CarModel> getCarModels() {
+        String jpql = "select cmd from CarModel cmd order by cmd.name";
+        TypedQuery<CarModel> query = em.createQuery(jpql, CarModel.class);
+        List<CarModel> result = query.getResultList();
+        return result;
+    }
 }
